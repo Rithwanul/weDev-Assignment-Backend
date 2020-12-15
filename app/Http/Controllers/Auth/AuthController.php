2f205570e47a4a9d6119cbc984a593d9c97d2544
Rithwan <Rithwan@DESKTOP-N1C8I9M>
2020-12-15 18:35:57 +0600

    first commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Notifications\SignupActivate;
+use Carbon\Carbon;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /**
+     * Create user
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse [string] message
+     */
+   public function SignUp(Request $request){
+
+       // Validation Rules
+       $rules = [
+           'name'           =>          'required|string',
+           'email'          =>          'required|string|email|unique:users',
+           'password'       =>          'required|string|confirmed'
+       ];
+
+       // Validating request with rules
+       $request->validate($rules);
+
+       // Create User model
+       $user = new User();
+       $user->name                  =           $request->name;
+       $user->email                 =           $request->email;
+       $user->password              =           bcrypt($request->password);
+       $user->activation_token      =           Str::random(60);
+       $user->save();
+
+       $mail = new SignupActivate($user);
+       $user->notify($mail);
+
+
+       return response()->json([
+           'message'        =>      'Successfully Created User'
+       ], 201);
+   }
+
+    /**
+     * Login user and create token
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse [string] access_token
+     */
+    public function login(Request $request){
+        // Validation rules
+        $rules  =   [
+            'email'         =>          'required|string|email',
+            'password'      =>          'required|string',
+            'remember_me'   =>          'boolean'
+        ];
+
+        //Validating request
+        $request->validate($rules);
+
+        // Setting Credentials for login
+        $credantials['email']           =           $request->email;
+        $credantials['password']        =           $request->password;
+        $credantials['active']          =           1;
+        $credantials['deleted_at']      =           null;
+
+
+        // Check is user authenticate or not
+        if (!Auth::attempt($credantials)){
+            return response()->json([
+                'message'       =>          'Unauthorized'
+            ], 401);
+        }
+
+        //creating token for the authenticate user
+        $user           =       $request->user();
+        $tokenResult    =       $user->createToken('Personal Access Token');
+        $token          =       $tokenResult->token;
+
+        //Setting time for JWT token
+        if($request->remember_me){
+            $token->expires_at      =           Carbon::now()->addWeeks(1);
+        }
+
+        $token->save();
+
+        return response()->json([
+            'access_token'      =>      $tokenResult->accessToken,
+            'token_type'        =>      'Bearer',
+            'expires_at'        =>      Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse [string] message
+     */
+    public function Logout(Request $request){
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message'       =>          'Successfully Logout'
+        ]);
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse [json] user object
+     */
+    public function User(Request $request){
+        return response()->json($request->user());
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse [json] user object
+     */
+    public function SignupActivate($token){
+
+        $user = User::where('activation_token', $token)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'This activation token is invalid.'
+            ], 404);
+        }
+
+        if($token == $user->activation_token){
+            $user->active = true;
+            $user->activation_token = '';
+        }else{
+            return response()->json([
+                'message'   =>  'Account not Activated'
+            ]);
+        }
+        $user->save();
+        return response()->json([
+            'message'       =>      'Account Activated!! Please Login'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\Auth\PasswordResetController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'prefix'    =>      'auth'
+], function (){
+    Route::post('login', [AuthController::class, 'Login']);
+    Route::post('signup', [AuthController::class, 'SignUp']);
+    Route::get('signup/activate/{token}', [AuthController::class, 'SignupActivate']);
+
+    Route::group([
+        'middleware'        =>      'auth:api'
+    ], function () {
+        Route::get('logout', [AuthController::class, 'Logout']);
+        Route::get('user', [AuthController::class, 'User']);
+    });
+});
+
+Route::group([
+    'namespace' => 'Auth',
+    'middleware' => 'api',
+    'prefix' => 'password'
+], function () {
+    Route::post('create', [PasswordResetController::class, 'Create']);
+    Route::get('find/{token}', [PasswordResetController::class, 'Find']);
+    Route::post('reset', [PasswordResetController::class, 'Reset']);
+});
